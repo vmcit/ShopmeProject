2f03d81f28cbbf68e43d019d6f1573e3285aa04a
wizeVuCuong <vmcit94@gmail.com>
2022-06-07 10:31:18 +0700

    commit task 3

--- ShopmeWebParent/ShopmeBackend/src/test/java/com/shopme/admin/user/UserRepositoryTests.java
+++ ShopmeWebParent/ShopmeBackend/src/test/java/com/shopme/admin/user/UserRepositoryTests.java
@@ -90,4 +90,32 @@ public class UserRepositoryTests {
 		repo.deleteById(userId);
 		
 	}
+	
+	@Test
+	public void testGetUserByEmail() {
+		String email = "ravi@gmail.com";
+		User user = repo.getUserByEmail(email);
+		
+		assertThat(user).isNotNull();
+	}
+	
+	@Test
+	public void testCountById() {
+		Integer id = 1;
+		Long countById = repo.countById(id);
+		
+		assertThat(countById).isNotNull().isGreaterThan(0);
+	}
+	
+	@Test
+	public void testDisableUser() {
+		Integer id = 1;
+		repo.updateEnabledStatus(id, false);
+	}
+	
+	@Test
+	public void testEnableUser() {
+		Integer id = 3;
+		repo.updateEnabledStatus(id, true);
+	}
 }
