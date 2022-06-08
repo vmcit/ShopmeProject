3beac4d83f861ebedb256b917db6b001b0d24808
wizeVuCuong <vmcit94@gmail.com>
2022-06-08 16:15:06 +0700

    comit search

--- ShopmeWebParent/ShopmeBackend/src/main/java/com/shopme/admin/user/UserController.java
+++ ShopmeWebParent/ShopmeBackend/src/main/java/com/shopme/admin/user/UserController.java
@@ -4,6 +4,8 @@ import java.io.IOException;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.util.StringUtils;
@@ -25,12 +27,45 @@ public class UserController {
 	private UserService service;
 	
 	@GetMapping("/users")
-	public String listAll(Model model) {
-		List<User> listUsers = service.listAll();
-		model.addAttribute("listUsers", listUsers);
-		
-		return "users";
+	public String listFirstPage(Model model) {
+		return listByPage(1, model, "firstName", "asc", null);
 	}
+	
+	@GetMapping("/users/page/{pageNum}")
+	public String listByPage(
+			@PathVariable(name = "pageNum") int pageNum, Model model,
+			@Param("sortField") String sortField, @Param("sortDir") String sortDir,
+			@Param("keyword") String keyword
+			) {
+		System.out.println("Sort Field: " + sortField);
+		System.out.println("Sort Order: " + sortDir);
+		
+		Page<User> page = service.listByPage(pageNum, sortField, sortDir, keyword);
+		
+		List<User> listUsers = page.getContent();
+		
+		long startCount = (pageNum - 1) * UserService.USERS_PER_PAGE + 1;
+		long endCount = startCount + UserService.USERS_PER_PAGE - 1;
+		if (endCount > page.getTotalElements()) {
+			endCount = page.getTotalElements();
+		}
+		
+		String reverseSortDir = sortDir.equals("asc") ? "desc" : "asc";
+		
+		model.addAttribute("currentPage", pageNum);
+		model.addAttribute("totalPages", page.getTotalPages());
+		model.addAttribute("startCount", startCount);
+		model.addAttribute("endCount", endCount);
+		model.addAttribute("totalItems", page.getTotalElements());
+		model.addAttribute("listUsers", listUsers);
+		model.addAttribute("sortField", sortField);
+		model.addAttribute("sortDir", sortDir);
+		model.addAttribute("reverseSortDir", reverseSortDir);
+		model.addAttribute("keyword", keyword);
+		
+		return "users";		
+	}
+	
 	
 	@GetMapping("/users/new")
 	public String newUser(Model model) {
--- ShopmeWebParent/ShopmeBackend/src/main/java/com/shopme/admin/user/UserService.java
+++ ShopmeWebParent/ShopmeBackend/src/main/java/com/shopme/admin/user/UserService.java
@@ -6,6 +6,10 @@ import java.util.NoSuchElementException;
 import javax.transaction.Transactional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
@@ -15,7 +19,8 @@ import com.shopme.common.entity.User;
 @Service
 @Transactional
 public class UserService {
-
+	public static final int USERS_PER_PAGE = 4;
+	
 	@Autowired
 	private UserRepository userRepo;
 	
@@ -27,6 +32,20 @@ public class UserService {
 	
 	public List<User> listAll() {
 		return (List<User>) userRepo.findAll();
+	}
+	
+	public Page<User> listByPage(int pageNum, String sortField, String sortDir, String keyword) {
+		Sort sort = Sort.by(sortField);
+		
+		sort = sortDir.equals("asc") ? sort.ascending() : sort.descending();
+				
+		Pageable pageable = PageRequest.of(pageNum - 1, USERS_PER_PAGE, sort);
+		
+		if (keyword != null) {
+			return userRepo.findAll(keyword, pageable);
+		}
+		
+		return userRepo.findAll(pageable);
 	}
 	
 	public List<Role> listRoles() {
