c24119ae4e3d3aba7f39607e8407bc6d12e3ae54
wizeVuCuong <vmcit94@gmail.com>
2022-06-12 13:06:14 +0700

    add login and authentication

--- ShopmeWebParent/ShopmeBackend/src/main/java/com/shopme/admin/security/WebSecurityConfig.java
+++ ShopmeWebParent/ShopmeBackend/src/main/java/com/shopme/admin/security/WebSecurityConfig.java
@@ -2,9 +2,13 @@ package com.shopme.admin.security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
@@ -13,13 +17,48 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
 	@Bean
-	public PasswordEncoder PasswordEncoder() {
+	public UserDetailsService userDetailsService() {
+		System.out.println("WebSecurityConfig" + "userDetailsService");
+		return new ShopmeUserDetailsService();
+	}
+	
+	@Bean
+	public PasswordEncoder passwordEncoder() {
+		System.out.println("WebSecurityConfig" + "passwordEncoder");
 		return new BCryptPasswordEncoder();
 	}
 	
+	public DaoAuthenticationProvider authenticationProvider() {
+		System.out.println("WebSecurityConfig" + "authenticationProvider");
+		DaoAuthenticationProvider authProvider = new DaoAuthenticationProvider();
+		authProvider.setUserDetailsService(userDetailsService());
+		authProvider.setPasswordEncoder(passwordEncoder());
+		
+		return authProvider;
+	}
+	
+	
+	
+	@Override
+	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+		System.out.println("WebSecurityConfig" + "authenticationProvider");
+		auth.authenticationProvider(authenticationProvider());
+	}
+
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
-		http.authorizeRequests().anyRequest().permitAll();
+		http.authorizeRequests()
+			.anyRequest().authenticated()
+			.and()
+			.formLogin()			
+				.loginPage("/login")
+				.usernameParameter("email")
+				.permitAll();
+	}
+
+	@Override
+	public void configure(WebSecurity web) throws Exception {
+		web.ignoring().antMatchers("/images/**", "/js/**", "/webjars/**");
 	}
 
 	
